ce86de56c4818152a102426424e2622bb980acb0
UrAltrEgo <92788105+lmen918@users.noreply.github.com>
2025-06-17 21:47:22 -0500

    Added Spring backend to the application.

---
+++ src/main/java/campaign_companion/CampaignCompanionApplication.java
@@ -0,0 +1,13 @@
+package campaign_companion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampaignCompanionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampaignCompanionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/campaign_companion/CampaignCompanionApplicationTests.java
@@ -0,0 +1,13 @@
+package campaign_companion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampaignCompanionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
